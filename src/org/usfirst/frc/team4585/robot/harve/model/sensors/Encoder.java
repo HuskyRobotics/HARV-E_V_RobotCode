d2f8348d7f45d9128285ba654ca4a8abef8264b3
zeajenson <zeajenson@gmail.com>
2017-02-14 10:08:15 -0700

    started working on code for the encoders

--- src/org/usfirst/frc/team4585/robot/harve/model/sensors/Encoder.java
+++ src/org/usfirst/frc/team4585/robot/harve/model/sensors/Encoder.java
@@ -1,5 +1,15 @@
 package org.usfirst.frc.team4585.robot.harve.model.sensors;
 
 public class Encoder {
-
+	private int port;
+	private double rotationsPerSecond;
+	
+	public Encoder(){
+		
+	}
+	public Encoder(int port){
+		
+	}
+	
+	
 }
