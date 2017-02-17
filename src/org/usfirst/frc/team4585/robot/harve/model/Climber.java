189a9988d12f28a91e3a3df3073d8b3ce61ba1ab
Bcpratt2000 <bcpratt2000@gmail.com>
2017-02-17 12:30:33 -0700

    Added Code to make the climber climb the climbable

--- src/org/usfirst/frc/team4585/robot/harve/model/Climber.java
+++ src/org/usfirst/frc/team4585/robot/harve/model/Climber.java
@@ -1,5 +1,23 @@
 package org.usfirst.frc.team4585.robot.harve.model;
 
+import edu.wpi.first.wpilibj.Spark;
+
 public class Climber {
+	
+	double speed=0;
+	int speedCoeff=1; //this is here to reverse the motor if needed
+	Spark motor;
+	
+	Climber(int port){
+		motor=new Spark(port);	
+	}
+	
+	void setClimb(boolean climb){
+		if(climb)
+			motor.set(speed*speedCoeff);
+	}
+	void setSpeed(double speed){
+		this.speed=speed;
+	}
 
 }
