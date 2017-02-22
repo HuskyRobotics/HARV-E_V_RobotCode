81a9e09068d08a0a9a1d6f8ad3fad9ec11c9aa3d
zeajenson <zeajenson@gmail.com>
2017-02-21 18:42:42 -0700

    added getters and setters

--- src/org/usfirst/frc/team4585/robot/harve/controller/HarvOperationController.java
+++ src/org/usfirst/frc/team4585/robot/harve/controller/HarvOperationController.java
@@ -26,6 +26,7 @@ public class HarvOperationController {
 	private double time;
 	private double millisPerIteration;
 	private double distanceToGoal;
+	private double xAimerIncorectness;
 	
 	private int weaponsTriger, weaponsToggle, weaponsAutoAlign, weaponsClimberToggle, weaponsClimberSpeedToggle;//undefiend buttons incase of useing a different controller.
 	private int driveXAxis, driveYAxis, driveZAxis;
@@ -39,6 +40,7 @@ public class HarvOperationController {
 	private boolean align;
 	private boolean setAlign;
 	private boolean isAligned;
+	private boolean canSeeTarget;
 	
 	public void HarvOperationController(){
 		drive = new DefaultDrive(0,1);
@@ -146,7 +148,9 @@ public class HarvOperationController {
 	}
 	
 	private void updateAutoAlign(){
-		
+		if(canSeeTarget){
+			magRot = 0.1 + xAimerIncorectness;
+		}
 	}
 	
 	private void augmentedDriveControll(){
@@ -165,4 +169,267 @@ public class HarvOperationController {
 		}
 	}
 
+	public HarvDrive getDrive() {
+		return drive;
+	}
+
+	public void setDrive(HarvDrive drive) {
+		this.drive = drive;
+	}
+
+	public HarvInput getDriveInput() {
+		return driveInput;
+	}
+
+	public void setDriveInput(HarvInput driveInput) {
+		this.driveInput = driveInput;
+	}
+
+	public HarvInput getWeaponsInput() {
+		return weaponsInput;
+	}
+
+	public void setWeaponsInput(HarvInput weaponsInput) {
+		this.weaponsInput = weaponsInput;
+	}
+
+	public SmartDashboard getDashboard() {
+		return dashboard;
+	}
+
+	public void setDashboard(SmartDashboard dashboard) {
+		this.dashboard = dashboard;
+	}
+
+	public Sensors getSensors() {
+		return sensors;
+	}
+
+	public void setSensors(Sensors sensors) {
+		this.sensors = sensors;
+	}
+
+	public HRLV_MaxSonar_EZ_Analog getSonar() {
+		return sonar;
+	}
+
+	public void setSonar(HRLV_MaxSonar_EZ_Analog sonar) {
+		this.sonar = sonar;
+	}
+
+	public Climber getClimber() {
+		return climber;
+	}
+
+	public void setClimber(Climber climber) {
+		this.climber = climber;
+	}
+
+	public Shooter getShooter() {
+		return shooter;
+	}
+
+	public void setShooter(Shooter shooter) {
+		this.shooter = shooter;
+	}
+
+	public Loader getLoader() {
+		return loader;
+	}
+
+	public void setLoader(Loader loader) {
+		this.loader = loader;
+	}
+
+	public double getMagX() {
+		return magX;
+	}
+
+	public void setMagX(double magX) {
+		this.magX = magX;
+	}
+
+	public double getMagY() {
+		return magY;
+	}
+
+	public void setMagY(double magY) {
+		this.magY = magY;
+	}
+
+	public double getMagRot() {
+		return magRot;
+	}
+
+	public void setMagRot(double magRot) {
+		this.magRot = magRot;
+	}
+
+	public double getRotLimit() {
+		return rotLimit;
+	}
+
+	public void setRotLimit(double rotLimit) {
+		this.rotLimit = rotLimit;
+	}
+
+	public double getTime() {
+		return time;
+	}
+
+	public void setTime(double time) {
+		this.time = time;
+	}
+
+	public double getMillisPerIteration() {
+		return millisPerIteration;
+	}
+
+	public void setMillisPerIteration(double millisPerIteration) {
+		this.millisPerIteration = millisPerIteration;
+	}
+
+	public double getDistanceToGoal() {
+		return distanceToGoal;
+	}
+
+	public void setDistanceToGoal(double distanceToGoal) {
+		this.distanceToGoal = distanceToGoal;
+	}
+
+	public double getxAimerIncorectness() {
+		return xAimerIncorectness;
+	}
+
+	public void setxAimerIncorectness(double xAimerIncorectness) {
+		this.xAimerIncorectness = xAimerIncorectness;
+	}
+
+	public int getWeaponsTriger() {
+		return weaponsTriger;
+	}
+
+	public void setWeaponsTriger(int weaponsTriger) {
+		this.weaponsTriger = weaponsTriger;
+	}
+
+	public int getWeaponsToggle() {
+		return weaponsToggle;
+	}
+
+	public void setWeaponsToggle(int weaponsToggle) {
+		this.weaponsToggle = weaponsToggle;
+	}
+
+	public int getWeaponsAutoAlign() {
+		return weaponsAutoAlign;
+	}
+
+	public void setWeaponsAutoAlign(int weaponsAutoAlign) {
+		this.weaponsAutoAlign = weaponsAutoAlign;
+	}
+
+	public int getWeaponsClimberToggle() {
+		return weaponsClimberToggle;
+	}
+
+	public void setWeaponsClimberToggle(int weaponsClimberToggle) {
+		this.weaponsClimberToggle = weaponsClimberToggle;
+	}
+
+	public int getWeaponsClimberSpeedToggle() {
+		return weaponsClimberSpeedToggle;
+	}
+
+	public void setWeaponsClimberSpeedToggle(int weaponsClimberSpeedToggle) {
+		this.weaponsClimberSpeedToggle = weaponsClimberSpeedToggle;
+	}
+
+	public int getDriveXAxis() {
+		return driveXAxis;
+	}
+
+	public void setDriveXAxis(int driveXAxis) {
+		this.driveXAxis = driveXAxis;
+	}
+
+	public int getDriveYAxis() {
+		return driveYAxis;
+	}
+
+	public void setDriveYAxis(int driveYAxis) {
+		this.driveYAxis = driveYAxis;
+	}
+
+	public int getDriveZAxis() {
+		return driveZAxis;
+	}
+
+	public void setDriveZAxis(int driveZAxis) {
+		this.driveZAxis = driveZAxis;
+	}
+
+	public boolean isFastClimber() {
+		return isFastClimber;
+	}
+
+	public void setFastClimber(boolean isFastClimber) {
+		this.isFastClimber = isFastClimber;
+	}
+
+	public boolean isChangeIsFastClimber() {
+		return changeIsFastClimber;
+	}
+
+	public void setChangeIsFastClimber(boolean changeIsFastClimber) {
+		this.changeIsFastClimber = changeIsFastClimber;
+	}
+
+	public boolean isShooting() {
+		return isShooting;
+	}
+
+	public void setShooting(boolean isShooting) {
+		this.isShooting = isShooting;
+	}
+
+	public boolean isChangeIsShooting() {
+		return changeIsShooting;
+	}
+
+	public void setChangeIsShooting(boolean changeIsShooting) {
+		this.changeIsShooting = changeIsShooting;
+	}
+
+	public boolean isAlign() {
+		return align;
+	}
+
+	public void setAlign(boolean align) {
+		this.align = align;
+	}
+
+	public boolean isSetAlign() {
+		return setAlign;
+	}
+
+	public void setSetAlign(boolean setAlign) {
+		this.setAlign = setAlign;
+	}
+
+	public boolean isAligned() {
+		return isAligned;
+	}
+
+	public void setAligned(boolean isAligned) {
+		this.isAligned = isAligned;
+	}
+
+	public boolean isCanSeeTarget() {
+		return canSeeTarget;
+	}
+
+	public void setCanSeeTarget(boolean canSeeTarget) {
+		this.canSeeTarget = canSeeTarget;
+	}
 }
