0a20130625432c34e276595ddbcd55815886d7d6
zeajenson <zeajenson@gmail.com>
2017-02-28 15:05:29 -0700

    made classes to listen to ports to get information from cameras and other devices

---
+++ src/org/usfirst/frc/team4585/robot/harve/model/intranet/NetPortListener.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team4585.robot.harve.model.intranet;
+
+public class NetPortListener {
+
+}
---
+++ src/org/usfirst/frc/team4585/robot/harve/model/intranet/SerialPortListener.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team4585.robot.harve.model.intranet;
+
+import javax.net.ssl.*;
+
+public class SerialPortListener {
+	
+}
